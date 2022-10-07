e0a04044fef0fc85a60b9feabf711ef7460d6c71
diazzzu <diazzz.usenov@gmail.com>
2022-10-07 16:54:46 +0300

    try10

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,6 @@ sudo apt update
 sudo apt install docker.io -y
 sudo apt install docker-compose -y
 pkill -f runserver
-
-sudo set -o allexport
-sudo source .ssh/environment
 sudo docker login -u $DOCKER_LOGIN -p $DOCKER_PWD
 sudo docker system prune --all -f
 sudo docker-compose down
