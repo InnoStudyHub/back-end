d2b5a41ae8cdf92e94681661c16e65562465b675
diazzzu <diazzz.usenov@gmail.com>
2022-10-07 14:03:10 +0300

    add action to deploy

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install docker.io -y
+sudo apt install docker-compose -y
+pkill -f runserver
+sudo docker login -u $DOCKER_LOGIN -p $DOCKER_PWD
+sudo docker system prune --all -f
+sudo docker-compose down
+sudo docker-compose pull
+sudo docker-compose up -d
