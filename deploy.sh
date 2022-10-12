653ad82ae7bebb07c5b1e427b9fc90e41f03abc9
diazzzu <diazzz.usenov@gmail.com>
2022-10-12 23:47:43 +0300

    docker compose change

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ pkill -f runserver
 
 sudo docker login -u $LOGIN -p $PWD
 sudo docker system prune --all -f
-sudo docker-compose down
+sudo docker-compose stop app
 sudo docker-compose pull
 sudo docker-compose up -d
