e1359d1215196dd18b52a35c4a1dd9675a1edbae
diazzzu <diazzz.usenov@gmail.com>
2022-10-13 01:59:49 +0300

    test

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ sudo docker login -u $LOGIN -p $PWD
 sudo docker system prune --all -f
 sudo docker-compose stop app
 sudo docker-compose pull
-sudo docker-compose up app -d
+sudo docker-compose up -d app
