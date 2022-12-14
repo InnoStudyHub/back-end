a9e231fa3f5646aff0cc5481cd93036657a31c90
diazzzu <diazzz.usenov@gmail.com>
2022-12-15 02:30:13 +0300

    try

--- studyhub/settings.py
+++ studyhub/settings.py
@@ -213,7 +213,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media_root'
 
