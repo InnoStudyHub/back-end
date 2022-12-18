0ccb02dcd2b9a719543ad0eaf5ebb2a3fc7f7fb0
diazzzu <diazzz.usenov@gmail.com>
2022-12-18 08:44:26 +0300

    add test action and fix tests

--- deck/urls/folder_urls.py
+++ deck/urls/folder_urls.py
@@ -4,5 +4,5 @@ from deck.views import FolderViewSet
 urlpatterns = [
     path('create/', FolderViewSet.as_view({"post": "create"}), name='folder_create'),
     path('list/', FolderViewSet.as_view({"get": "list"}), name='folder_list'),
-    path('get/', FolderViewSet.as_view({"get": "get"}), name='folder_list'),
+    path('get/', FolderViewSet.as_view({"get": "get"}), name='folder_get'),
 ]
--- user/tests.py
+++ user/tests.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
 from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
+from rest_framework_api_key.models import APIKey
 
 from user.models import User
 
@@ -15,31 +16,35 @@ class UserAuthTests(APITestCase):
         cls.login_data = [{'email': 'test@test.com', 'password': 'pass1234'},
                           {'email': 'nottest@test.com', 'password': 'pass1234'}]
 
+    def setUp(self):
+        api_key, key = APIKey.objects.create_key(name="test-api-key")
+        self.api_key_headers = {'HTTP_X_API_KEY': key}
+
     def test_successful_register_user(self):
         url = reverse('user_register')
-        response = self.client.post(url, self.register_data[0], format='json')
+        response = self.client.post(url, data=self.register_data[0], format='json', **self.api_key_headers)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_existing_register_user(self):
         url = reverse('user_register')
-        self.client.post(url, self.register_data[0], format='json')
-        response = self.client.post(url, self.register_data[1], format='json')
+        self.client.post(url, self.register_data[0], format='json', **self.api_key_headers)
+        response = self.client.post(url, self.register_data[1], format='json', **self.api_key_headers)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_login_user(self):
-        self.client.post(reverse('user_register'), self.register_data[0], format='json')
-        response = self.client.post(reverse('user_login'), self.login_data[0], format='json')
+        self.client.post(reverse('user_register'), self.register_data[0], format='json', **self.api_key_headers)
+        response = self.client.post(reverse('user_login'), self.login_data[0], format='json', **self.api_key_headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_login_not_existing_user(self):
-        self.client.post(reverse('user_register'), self.register_data[0], format='json')
-        response = self.client.post(reverse('user_login'), self.login_data[1], format='json')
+        self.client.post(reverse('user_register'), self.register_data[0], format='json', **self.api_key_headers)
+        response = self.client.post(reverse('user_login'), self.login_data[1], format='json', **self.api_key_headers)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_logout_user(self):
-        token = self.client.post(reverse('user_register'), self.register_data[0], format='json').data
+        token = self.client.post(reverse('user_register'), self.register_data[0], format='json', **self.api_key_headers).data
         auth_header = {'HTTP_AUTHORIZATION': f'Bearer {token["access"]}'}
-        response = self.client.post(reverse('user_logout'), format='json', **auth_header)
+        response = self.client.post(reverse('user_logout'), format='json', **auth_header, **self.api_key_headers)
         user = User.objects.get(email='test@test.com')
         outstanding_token = OutstandingToken.objects.filter(user=user)
         blacklisted_token = BlacklistedToken.objects.filter(id=outstanding_token[0].id)
@@ -47,6 +52,6 @@ class UserAuthTests(APITestCase):
         self.assertIsNotNone(blacklisted_token)
 
     def test_login_refresh(self):
-        token = self.client.post(reverse('user_register'), self.register_data[0], format='json').data
-        response = self.client.post(reverse('token_refresh'), {'refresh': token['refresh']}, format='json')
+        token = self.client.post(reverse('user_register'), self.register_data[0], format='json', **self.api_key_headers).data
+        response = self.client.post(reverse('token_refresh'), {'refresh': token['refresh']}, format='json', **self.api_key_headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
