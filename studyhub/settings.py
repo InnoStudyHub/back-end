68c8238f23abe6d4af2a5064f4a961a923ea50e3
diazzzu <diazzz.usenov@gmail.com>
2022-12-20 07:45:26 +0300

    try9

--- studyhub/settings.py
+++ studyhub/settings.py
@@ -55,7 +55,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-mw_n2-#0p-4p-a
 #Google Cloud settings
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', '.data/google_cloud_key.json')
 GS_CREDENTIALS = service_account.Credentials.from_service_account_file(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
-GS_STATIC_BUCKET_NAME = 'studyhub-static'
+GS_BUCKET_NAME = 'studyhub-static'
 GS_DATA_BUCKET_NAME = os.environ.get('BUCKET_NAME', 'studyhub-data-dev')
 GS_PROJECT_ID = 'studyhub-372200'
 
@@ -222,7 +222,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 DEFAULT_FILE_STORAGE = 'studyhub.storage.GoogleCloudMediaStorage'
 STATICFILES_STORAGE = 'studyhub.storage.GoogleCloudStaticStorage'
-STATIC_URL = f'https://storage.googleapis.com/{GS_STATIC_BUCKET_NAME}/'
+STATIC_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
