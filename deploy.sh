c41433861a440d67cd639887764e0faee3489c20
diazzzu <diazzz.usenov@gmail.com>
2023-05-02 15:20:31 +0300

    test new server 2

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ pkill -f runserver
 
 sudo docker login -u $DOCKER_LOGIN -p $DOCKER_PASSWORD
 sudo docker system prune --all -f
-sudo docker compose down
-sudo docker compose pull
-sudo docker compose up -d
+sudo docker-compose down
+sudo docker-compose pull
+sudo docker-compose up -d
